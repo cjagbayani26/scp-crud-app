41130de6a0632b4ced687092146eb9fe9ef3316d
cjagbayani26 <30024651@student.toiohomai.ac.nz>
2023-11-20 01:33:27 +1300

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import AddUpdate from './pages/AddUpdate';
+import View from './pages/View';
+import About from './pages/About';
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer position="top-center"/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/add" element={<AddUpdate/>}/>
+        <Route path="/update/:id" element={<AddUpdate/>}/>
+        <Route path="/view/:id" element={<View/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
